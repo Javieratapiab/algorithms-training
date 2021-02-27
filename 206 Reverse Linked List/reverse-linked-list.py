6e46dd920ad512be99dccaec565fcbde025dc33a
javieratapiab <javieratapiabobadilla@gmail.com>
2021-02-27 15:10:23 -0300

    feat: added Youtube explanation to exercise 206.

--- 206 Reverse Linked List/reverse-linked-list.py
+++ 206 Reverse Linked List/reverse-linked-list.py
@@ -1,11 +1,12 @@
 # LeetCode problem (Easy)
 # https://leetcode.com/problems/reverse-linked-list/
-# Definition for singly-linked list.
+# Explanation: https://www.youtube.com/watch?v=NhapasNIKuQ
 
-class ListNode(object):
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
+# Definition for singly-linked list.
+# class ListNode(object):
+#    def __init__(self, val=0, next=None):
+#        self.val = val
+#        self.next = next
 
 
 class Solution(object):
