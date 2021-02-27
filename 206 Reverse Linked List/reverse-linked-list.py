991d321f8bb2acb552118a6dba580571806bcc03
javieratapiab <javieratapiabobadilla@gmail.com>
2021-02-27 14:42:43 -0300

    fix: rename folders as convention. Added ex. 205 (Linked list)

---
+++ 206 Reverse Linked List/reverse-linked-list.py
@@ -0,0 +1,24 @@
+# LeetCode problem (Easy)
+# https://leetcode.com/problems/reverse-linked-list/
+# Definition for singly-linked list.
+
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution(object):
+    def reverseList(self, head: ListNode) -> ListNode:
+        # Base cases
+        if head == None or head.next == None:
+            return head
+        prev = None
+        curr = head
+
+        while(curr != None):
+            next_temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next_temp
+        return prev
